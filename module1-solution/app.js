4e0a564afb512bf248f9296db281b60e809468e3
Agnieszka Klosak <agne33@interia.pl>
2017-04-30 21:44:34 +0200

    new content addes

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,47 +1,34 @@
 (function(){
 'use strict';
 
-    angular.module('lunchMenuApp', [])
-        .controller('menuItemsController', menuItemsController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-    menuItemsController.$inject = ['$scope'];
-    function menuItemsController($scope){
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope){
         $scope.comma = ',';
         $scope.item = '';
         $scope.arrayOfStrings = [];
         $scope.state = "";
-        $scope.count = 0;
-    
+            
 
         $scope.checkIfTooMuch = function(){
-            //console.log($scope.arrayOfStrings.length);
             if ($scope.item === ''){
                 $scope.state = "Please enter data first";
             }
 
             else{
                 $scope.arrayOfStrings = ($scope.item).split($scope.comma);
-                //console.log($scope.arrayOfStrings.length);
-                //$scope.count = $scope.arrayOfStrings.length;
-
+            
                 if (($scope.arrayOfStrings).length > 3){
                     $scope.state = "Too Much!";
-                    $scope.count = $scope.arrayOfStrings.length;
-                    
+                    $scope.count = $scope.arrayOfStrings.length;   
                 }
-                /*
-                if ((($scope.arrayOfStrings).length) < 1){
-                    $scope.state = "Please enter data first";
-                    //$scope.count = $scope.arrayOfStrings.length;
-                }
-                */
-
+            
                 if ((($scope.arrayOfStrings).length <= 3) && (($scope.arrayOfStrings).length >= 1)){
                     $scope.state = "Enjoy!";  
                     $scope.count = $scope.arrayOfStrings.length;
                 }
-
-                //$scope.count = 0;
             }
         }
     }
